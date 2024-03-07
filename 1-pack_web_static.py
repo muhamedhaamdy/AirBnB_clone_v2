9d7e2facb8ee57b25e2924dfeb3a2c653743d772
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-08 01:20:54 +0200

    "hotfix"

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 '''script that generates a .tgz archive from the contents of the web_static'''
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     '''Generates a .tgz archive from the contents of the web_static'''
-    source_dir = 'web_static'
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    output_filename = 'web_static_{}.tgz'.formate(timestamp)
-    local('mkdir -p versions')
-    local('tar -cvzf  q versons/{} {}'.format(output_filename, source_dir))
-    return 'versions/{}'.format(output_filename)
+    try:
+    	source_dir = 'web_static'
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        output_filename = 'web_static_{}.tgz'.formate(timestamp)
+        local('mkdir -p versions')
+        check = local('tar -cvzf versons/{} {}'.format(output_filename, source_dir))
+        return 'versions/{}'.format(output_filename)
+    except Exception:
+        return None
