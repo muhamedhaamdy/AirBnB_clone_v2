cd8b26c4f37ea98dcdf82564426a6040e3d3a46d
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-08 01:37:42 +0200

    "hotfix"

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,8 @@ def do_pack():
     '''Generates a .tgz archive from the contents of the web_static'''
     try:
         local('mkdir -p versions')
-        output_path = 'versions/web_static_{}.tgz'.formate(time.strftime('%Y%m%d%H%M%S'))
+        output_path = 'versions/web_static_{}.tgz'.formate(
+            time.strftime('%Y%m%d%H%M%S'))
         check = local('tar -cvzf {} web_static'.format(output_path))
         return output_path
     except Exception:
