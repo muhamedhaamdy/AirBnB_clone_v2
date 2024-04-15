8d8b9fd25d47b9fa8c308455ff3f0a00739faae6
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-15 08:22:23 +0200

    "hotfix"

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
+def hello_HBNB():
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def just_HBNB():
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run()
