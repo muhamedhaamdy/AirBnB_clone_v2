98ca79d5208c04dc19db45d7832f9bc156ed2a68
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-15 08:46:35 +0200

    "hotfix"

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
 """Fabric script module"""
 from fabric.api import *
+import paramiko
 import time
-import os
-
-env.hosts = ['3.90.84.182', '54.227.198.43']
-env.user = 'ubuntu'
 
 
-def do_deploy(archive_path):
-    """Distributes an archive to my web servers"""
-    if not os.path.exists(archive_path):
-        return False
-
-    archive_file = archive_path[9:]
-    release_version = '/data/web_static/releases/{}'.format(archive_file[:-4])
-
-    # archive_file: web_static_20231210153350.tgz
-    # release_version: /data/web_static/releases/web_static_20231210153350
-
-    put(archive_path, '/tmp/')
-    run('mkdir -p {}'.format(release_version))
-    run('tar -xzf /tmp/{} -C {}'.format(archive_file, release_version))
-    run('rm /tmp/{}'.format(archive_file))
-    run('mv {}/web_static/* {}'.format(release_version, release_version))
-    run('rm -rf /data/web_static/releases/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s {} /data/web_static/current'.format(release_version))
-
-    print('New version deployed!')
-    return True
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static"""
+    try:
+        local("mkdir -p versions")
+        archive_path = 'versions/web_static_{}.tgz'.format(
+                time.strftime('%Y%m%d%H%M%S'))
+        results = local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except Exception:
+        return None
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''first task'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_HBNB():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    text_with_spaces = text.replace('_', ' ')
+    return "C {}".format(text_with_spaces)
+
+
+if __name__ == "__main__":
+    app.run()
