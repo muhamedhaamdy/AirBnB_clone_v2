903bc4235658f39bf4c203a8e028907e88c270f6
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-16 13:36:43 +0200

    "hotfix"

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,56 @@
 #!/usr/bin/python3
 '''first task'''
 from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import render_template
 
 app = Flask(__name__)
 
-all_states = storage.all(State)
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def just_HBNB():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    text_with_spaces = text.replace('_', ' ')
+    return "C {}".format(text_with_spaces)
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text):
+    text_with_spaces = text.replace('_', ' ')
+    return "Python {}".format(text_with_spaces)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_num(n):
+    return '{} is a number'.format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    return render_template('5-number.html', num=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def even_or_odd(n):
+    return render_template('6-number_odd_or_even.html', num=n)
+
+
+
 @app.route("/states_list", strict_slashes=False)
-def list_of_cities(all_states):
-    return render_template('7-states_list.html', all_states=all_states)
+def list_of_cities():
+    states = [state for state in storage.all(State).values()]
+    return render_template('7-states_list.html', all_states=states)
 
 
 @app.teardown_appcontext
